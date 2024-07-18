50a2cf2074b424cd982670c8c987d5c1db8db43e
ram kador <ram.kador@gmail.com>
2024-07-18 11:34:17 +0300

    amir forced me

--- front.js
+++ front.js
@@ -1,4 +1,14 @@
-function changeBG(color){ 	 	
-	 document.body.style.backgroundColor = color; 
+videos = ["https://www.youtube.com/watch?v=WjQgsyPkuCk",,"https://www.youtube.com/watch?v=VFIGj5f8G1k"]
+function changeBG(){ 
+
+	 if (document.body.style.backgroundColor == 'lightblue') 
+	 	document.body.style.backgroundColor = 'lightgray';
+	 else
+	 	document.body.style.backgroundColor = 'lightblue';
+
 
 }
+function playMusic(){
+  var music = new Audio('song.mp3');
+  music.play();
+  }
